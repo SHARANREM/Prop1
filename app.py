b69201082ec4dc4d8c4148db94d1e9ccfedfef36
SHARANREM <sharanplm@gmail.com>
2025-06-23 07:43:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import platform
 from flask_cors import CORS
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 @app.route('/')
 def index():
     return render_template('index.html')
